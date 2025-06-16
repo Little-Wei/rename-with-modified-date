9e494c024fe0df3a7a6925f7aeeed4e394f1c561
Little-Wei <william1205123@gmail.com>
2025-06-16 23:10:54 +0800

    加入檔案

---
+++ rename-app/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+//string folderPath = Directory.GetCurrentDirectory();
+string folderPath = @"C:\Users\William\OneDrive\桌面\Web\Rename-App\100ANDRO"; // Change this to your target folder path
+if (!Directory.Exists(folderPath))
+{
+    Console.WriteLine("指定的資料夾不存在。");
+    return;
+}
+
+var files = Directory.GetFiles(folderPath);
+
+foreach (var file in files)
+{
+    var info = new FileInfo(file);
+    var lastWrite = info.LastWriteTime;
+    string newFileName = lastWrite.ToString("yyyyMMdd_HHmmss") + info.Extension;
+    //Console.WriteLine($"{info.Name} -> {newFileName}");
+    string newPath = Path.Combine(folderPath, newFileName);
+    if (!File.Exists(newPath))
+    {
+        File.Move(file, newPath);
+        Console.WriteLine($"{info.Name} -> {newFileName}");
+    }
+    else
+    {
+        Console.WriteLine($"{newFileName} 已存在，略過 {info.Name}");
+    }
+}
